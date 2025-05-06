30a03f21226fdea59fee31371e5f2890e807f0a5
hossamhassan77 <h.nasr@wakeb.tech>
2025-05-06 15:28:02 +0300

    upload react as js file formats

---
+++ learn-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import Hello from './Components/Greet';
+import {Greet, Hello} from './Components/Greet'
+function App() {
+  return (
+    <div className="App">
+        <Greet />
+        <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ learn-react/src/Components/Greet.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hello = () => <p>How r u 3m meso?</p>;
+function Greet() {
+  return <h1>Hello meso!</h1>;
+}
+export { Hello, Greet };
