635545fefe8edadad5bfb62616e75e0a26501871
hossamhassan77 <h.nasr@wakeb.tech>
2025-05-11 09:48:25 +0300

    dynamic popup is on

--- learn-react/src/components/MapView.jsx
+++ learn-react/src/components/MapView.jsx
@@ -9,29 +9,51 @@ export default function MapView() {
       [
         'esri/Map',
         'esri/views/MapView',
-        'esri/layers/FeatureLayer',
-        "esri/renderers/SimpleRenderer",
-        "esri/symbols/SimpleFillSymbol"
+        'esri/layers/FeatureLayer'
       ],
       { css: true }
-    ).then(([Map, MapView, FeatureLayer, SimpleRenderer, SimpleFillSymbol]) => {
+    ).then(([Map, MapView, FeatureLayer]) => {
       const map = new Map({
-        basemap: 'satellite',
+        basemap: 'gray-vector',
       });
-      const symbol = new SimpleFillSymbol({
-        color: [0, 120, 200, 0.5], // RGBA: blue fill with transparency
-        outline: {
-          color: [0, 0, 0, 1],
-          width: 1
+      const renderer = {
+        type: "simple",
+        symbol: {
+          type: "simple-marker",
+          size: 6,
+          color: "black",
+          outline: {
+            width: 0.5,
+            color: "white"
+          }
         }
-      });
-
-      const renderer = new SimpleRenderer({
-        symbol
-      });
+      }
       const raptorNestLayer = new FeatureLayer({
         url: 'https://services5.arcgis.com/A7KxbLP4BqGmGXEY/arcgis/rest/services/raptorNests/FeatureServer/0',
-        renderer,
+        outFields: ['*'],
+        renderer: renderer,
+      });
+      // Dynamically generate popup
+      raptorNestLayer.when(() => {
+        const fieldInfos = raptorNestLayer.fields
+          .filter(f => !["OBJECTID", "Shape", "GlobalID"].includes(f.name))
+          .map(f => ({
+            fieldName: f.name,
+            label: f.alias || f.name,
+            format:
+              f.type === "date"
+                ? { dateFormat: "short-date" }
+                : f.type === "double" || f.type === "integer"
+                ? { digitSeparator: true, places: 2 }
+                : undefined
+          }));
+        raptorNestLayer.popupTemplate = {
+          title: raptorNestLayer.title || "Feature Info",
+          content: [{
+            type: "fields",
+            fieldInfos
+          }]
+        };
       });
       map.add(raptorNestLayer);
       view = new MapView({
