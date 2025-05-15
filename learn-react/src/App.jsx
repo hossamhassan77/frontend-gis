add9a55e073b0cf22a288e1b35bf0dd53df85993
hossamhassan77 <h.nasr@wakeb.tech>
2025-05-15 11:28:45 +0300

    passing layerID

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -4,7 +4,7 @@ import MapWithMapImageLayer from './components/mapImage';
 function App() {
   return (
     <div className="App">
-      <MapWithMapImageLayer />
+      <MapWithMapImageLayer layerID={2}/>
       <div id='map-info'>
         <p><strong>Curser location</strong></p>
       </div>
